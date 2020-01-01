595100efb89ac1b091f51a87f5d9770b51b8d8f8
Shota Maekawa <f22raptor0817@gmail.com>
2020-01-01 22:40:45 +0900

    Add routing that check the app server

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import json
+
 import tornado.websocket
 import tornado.web
 import tornado.ioloop
+import tornado.escape
+
+import requests
 
 
 class WebSocket(tornado.websocket.WebSocketHandler):
@@ -15,11 +20,28 @@ class WebSocket(tornado.websocket.WebSocketHandler):
         return True
 
     def on_message(self, message):
-        print(message)
-        self.write_message("Hi")
+        print("message")
+        data = json.loads(message)
+        print(data)
 
 
-app = tornado.web.Application([(r"/", WebSocket)])
+class CheckAppUrlHandler(tornado.web.RequestHandler):
+    def post(self):
+        try:
+            data = tornado.escape.json_decode(self.request.body)
+            response = requests.get(data["url"])
+            res = True if response.text=="true" else False
+        except:
+            res = False
+
+        return_value = {"status": res}
+        self.write(json.dumps(return_value))
+
+
+app = tornado.web.Application([
+    (r"/", WebSocket),
+    (r"/check_url", CheckAppUrlHandler),
+])
 
 
 if __name__ == "__main__":
